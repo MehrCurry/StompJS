7e4df3f55780d04deafb0d4df1353ac0fef40ce1
Guido Zockoll <gzockoll@gmail.com>
2012-02-04 08:58:16 +0100

    Working example with ActiveMQ

--- js/init.js
+++ js/init.js
@@ -45,38 +45,116 @@ function init() {
 		ledColor : steelseries.LedColor.YELLOW_LED,
 	});
 
-	// Let's set some values...
-	radial1.setValueAnimated(70);
-	radial2.setValueAnimated(41);
-	radial3.setValueAnimated(70);
+	var radial4 = new steelseries.Radial('canvas4', {
+		titleString : 'Temperatur',
+		unitString : '°C',
+		minValue: -50,
+		maxValue: 50,
+		frameDesign : steelseries.FrameDesign.BLACK_METAL,
+		backgroundColor : steelseries.BackgroundColor.LIGHT_GRAY,
+		valueColor : steelseries.ColorDef.YELLOW,
+		lcdColor : steelseries.LcdColor.YELLOW,
+		ledColor : steelseries.LedColor.YELLOW_LED,
+	});
 
+	var radial5 = new steelseries.Radial('canvas5', {
+		titleString : 'Luftdruck',
+		unitString : 'hPa',
+		minValue: 900,
+		maxValue: 1100,
+		frameDesign : steelseries.FrameDesign.BLACK_METAL,
+		backgroundColor : steelseries.BackgroundColor.LIGHT_GRAY,
+		valueColor : steelseries.ColorDef.YELLOW,
+		lcdColor : steelseries.LcdColor.YELLOW,
+		ledColor : steelseries.LedColor.YELLOW_LED,
+	});
+
+	var radial6 = new steelseries.Radial('canvas6', {
+		titleString : 'Wind',
+		unitString : 'm/s',
+		minValue: 0,
+		maxValue: 50,
+		frameDesign : steelseries.FrameDesign.BLACK_METAL,
+		backgroundColor : steelseries.BackgroundColor.LIGHT_GRAY,
+		valueColor : steelseries.ColorDef.YELLOW,
+		lcdColor : steelseries.LcdColor.YELLOW,
+		ledColor : steelseries.LedColor.YELLOW_LED,
+	});
+
+	var radial7 = new steelseries.Compass('canvas7', {
+		titleString : 'Windrichtung',
+		unitString : '°',
+		frameDesign : steelseries.FrameDesign.BLACK_METAL,
+		backgroundColor : steelseries.BackgroundColor.LIGHT_GRAY,
+		valueColor : steelseries.ColorDef.YELLOW,
+		lcdColor : steelseries.LcdColor.YELLOW,
+		ledColor : steelseries.LedColor.YELLOW_LED,
+	});
+
+	var dashboard = new Object();
+	dashboard["Test1"]=radial1;
+	dashboard["Test2"]=radial2;
+	dashboard["Test3"]=radial3;
+	dashboard["Temperatur"]=radial4;
+	dashboard["Luftdruck"]=radial5;
+	dashboard["Wind"]=radial6;
+	dashboard["Windrichtung"]=radial7;
+	
+	
 	var url = "ws://localhost:61614/stomp";
 	var client = Stomp.client(url);
+	var login = "";
+	var passcode = "";
 
-	client.connect(login, passcode, connect_callback, error_callback);
+	radial2.setValueAnimated(20);
+	
+	try {
+		error_callback = function(error) {
+			// display the error's message header:
+			alert(error.headers.message);
+		};
 
-	error_callback = function(error) {
-		// display the error's message header:
-		alert(error.headers.message);
-	};
+		connect_callback = function() {
+			client.send("/queue/test", {priority: 9}, "Hello, Stomp");
+			id = client.subscribe("/topic/observations", callback);
+			// alert("Connected!");
+		};
 
-	connect_callback = function() {
-		alert("Connected!");
-	};
+		client.connect(login, passcode, connect_callback, error_callback);
+		radial2.setValueAnimated(30);
 
-	id = client.subscribe("/topic/observations", callback);
 
-	callback = function(message) {
-		// called when the client receives a Stomp message from the server
-		if (message.body) {
-			alert("got message with body " + message.body)
-		} else {
-			alert("got empty message");
-		}
+		callback = function(message) {
+			// called when the client receives a Stomp message from the server
+			if (message.body) {
+				// alert("got message with body " + message.body);
+				var data = message.body;
+                var payload = jQuery.parseJSON(data);
+                radial=dashboard[payload.key];
+                if (radial != undefined && payload.val != undefined && !isNaN(payload.val)) {
+                    radial.setValueAnimated(payload.val);
+                } else {
+                    console.log(payload.radialvalue + " not a number");
+                }
+			} else {
+				alert("got empty message");
+			}
+		};
+
+		radial2.setValueAnimated(40);
 	}
 
-	client.disconnect(function() {
-		alert("See you next time!");
-	})
+	catch (e) {
+
+		alert("Fehler: " + e);
+
+	}
+
+	finally {
+//		client.disconnect(function() {
+//			alert("See you next time!");
+//		});
+		radial2.setValueAnimated(70);
+	}
 
 }
